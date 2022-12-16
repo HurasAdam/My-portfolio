daeeb622a15cb6c2f66a77b50afeab79f7b2c16c
HurasAdam <huradam@wp.pl>
2022-12-16 18:32:22 +0100

    update

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+const burgerButton =document.querySelector('.burger');
+const mobileMenu= document.querySelector('.menu');
+
+
+
+
+
+
+
+function MenuTrigger(){
+
+    mobileMenu.classList.toggle('active');
+    burgerButton.classList.toggle('active');
+}
+
+burgerButton.addEventListener('click',MenuTrigger);
