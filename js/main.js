3ce96049e95171e5c5421ee8c120e5a7f0a9b0b5
Huras Adam <huradam@wp.pl>
2022-12-26 00:21:44 +0100

    update

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const desc = document.querySelector(".description");
 const home = document.querySelector(".home");
 const project = document.querySelector(".projects");
 const projectContent = document.querySelector(".projects-content");
-
+const technologySection= document.querySelector('.technologies-items')
 const headeroptions = {
   root: null,
   rootMarin: "0px",
@@ -102,10 +102,49 @@ const test = document.getElementById('test')
 
 
 
-for(i=0;i<technologiesItem.length;i++){
-  technologiesItem[i].classList.add('active')
-// technologiesItem[i].style.opacity='1';
-technologiesItem[i].style.transitionDelay = `${i*(0.7)}s`
-technologiesItem[i].style.transitionDuration = `${4}s`
+
+
+// function animateTechnologySection(entries){
+// entries.forEach(entry=>{
+
+//   if(entry.isIntersecting){
+//     for(i=0;i<technologiesItem.length;i++){
+//       technologiesItem[i].classList.add('active')
+//     // technologiesItem[i].style.opacity='1';
+//     technologiesItem[i].style.transitionDelay = `${i*(0.7)}s`
+//     technologiesItem[i].style.transitionDuration = `${4}s`
+//     }
+//   }
+// })
+// }
+
+
+const techOptions={
+
+  root:null,
+  rootMarin:"0px",
+  threshold:0.1
 }
+
+const chuj= new IntersectionObserver(function (entries, observer) {
+  entries.forEach((entry) => {
+ 
+    if (entry.isIntersecting) {
+      for(i=0;i<technologiesItem.length;i++){
+        technologiesItem[i].classList.add('active')
+      // technologiesItem[i].style.opacity='1';
+      technologiesItem[i].style.transitionDelay = `${i*(0.4)}s`
+      technologiesItem[i].style.transitionDuration = `${1.6}s`
+    }
+    }
+  });
+}, techOptions);
+chuj.observe(technologySection)
+
+
+
+
+
+
+
   
